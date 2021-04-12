f20d3034f780a257f2d82547941e534d84dbb960
Petr Severa <segedacz@gmail.com>
2021-04-12 18:44:05 +0200

    transfer query

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ Transcribe ČNB Exchange Rate from XML to JSON
 `index.js` is the content of the Workers script.
 
 [Running script](https://cnb-exchange-rate.adresa.workers.dev)
-
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const url = 'https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.xml'
+const url =
+  'https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.xml'
 
 const kurzy = {}
 
@@ -16,29 +17,31 @@ class RadekElementHandler {
     kurz.kod = element.getAttribute('kod')
     kurz.mena = element.getAttribute('mena')
     kurz.mnozstvi = Number(element.getAttribute('mnozstvi'))
-    kurz.kurz = Number(element.getAttribute('kurz').replace(',','.'))
+    kurz.kurz = Number(element.getAttribute('kurz').replace(',', '.'))
     kurz.zeme = element.getAttribute('zeme')
     kurzy.kurzy.push(kurz)
   }
 }
 
-async function handleRequest() {
-  const response = await fetch(url)
+async function handleRequest(request) {
+  const { search } = new URL(request.url)
+
+  const response = await fetch(url + search)
   await new HTMLRewriter()
-    .on("kurzy", new KurzyElementHandler())
-    .on("radek", new RadekElementHandler())
+    .on('kurzy', new KurzyElementHandler())
+    .on('radek', new RadekElementHandler())
     .transform(response)
     .arrayBuffer()
 
   return new Response(JSON.stringify(kurzy), {
     headers: {
-      "content-type": "application/json;charset=UTF-8",
+      'content-type': 'application/json;charset=UTF-8',
       'access-control-allow-origin': '*',
       'cnb-exchange-rate': 'json',
     },
   })
 }
 
-addEventListener("fetch", event => {
-  return event.respondWith(handleRequest())
+addEventListener('fetch', event => {
+  return event.respondWith(handleRequest(event.request))
 })
